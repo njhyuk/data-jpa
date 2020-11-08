ea0e7dfac5d9d22260e2db30c43d3d9b4c9c4a38
njhyuk <njh2239@gmail.com>
2020-11-09 00:36:58 +0900

    페이징

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -30,6 +33,15 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("member1", 10));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("member" + i, 10));
+        }
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+//        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
     }
 }
